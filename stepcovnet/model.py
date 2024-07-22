915262bebdd586593d48e0fa773a3826fca834ad
Alexander Hartl <me@alexhartl.eu>
2024-07-22 09:30:32 +0200

    Bug fix for keras model loading

--- stepcovnet/model.py
+++ stepcovnet/model.py
@@ -132,7 +132,7 @@ class ClassifierModel(AbstractModel):
 class GPT2ArrowModel(ArrowModel):
     def _create_arrow_model(self, arrow_input: Input, arrow_mask: Input) -> Layer:
         gp2_model = PretrainedModels.gpt2_model()
-        model_output = gp2_model(arrow_input, attention_mask=arrow_mask)[0]
+        model_output = gp2_model.transformer(arrow_input, attention_mask=arrow_mask)[0]
         # GPT-2 model returns feature maps for avg/max pooling. Using LSTM for additional feature extraction.
         # Might be able to replace this with another method in the future
         return GlobalMaxPool1D()(model_output)
